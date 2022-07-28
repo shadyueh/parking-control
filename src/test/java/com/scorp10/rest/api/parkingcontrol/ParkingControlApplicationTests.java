f34cca57173612815bd787b7debd51d9a7ee4b1b
Held Grijo <held.grijo@manaus.am.gov.br>
2022-07-28 14:25:35 -0400

    chore: cria base do projeto Java
    utilizando o spring initialzr (https://start.spring.io/)

---
+++ src/test/java/com/scorp10/rest/api/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorp10.rest.api.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

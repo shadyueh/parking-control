f13f0748ac3e5a50f55f86afd246c5ada9c93791
Held Grijo <held.grijo@gmail.com>
2022-07-29 16:48:01 -0400

    feat: adiciona endpoint para teste

--- src/main/java/com/scorp10/rest/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/scorp10/rest/api/parkingcontrol/ParkingControlApplication.java
@@ -2,9 +2,17 @@ package com.scorp10.rest.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "<h1>It's working!</h1><p>Spring Boot Stub App is OK!</p>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
